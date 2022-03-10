4169d60ad8c5979bf7ac71c40031d734f8343ff7
duongmoe174 <duongmoe174@gmail.com>
2022-03-10 14:13:22 +0700

    update Mouse

--- src/model/Mouse.java
+++ src/model/Mouse.java
@@ -1,4 +1,51 @@
 package model;
 
-public class Mouse {
+public class Mouse extends Pet implements Discount {
+    private double size;
+
+    public Mouse() {
+    }
+
+    public Mouse(String id, String name, int age, String gender, double weight, String color, Breed breed, double price, double size) {
+        super(id, name, age, gender, weight, color, breed, price);
+        this.size = size;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "Mouse{" +
+                ", id='" + getId() + '\'' +
+                ", name='" + getName() + '\'' +
+                ", age=" + getAge() +
+                ", gender=" + getGender() +
+                ", weight=" + getWeight() +
+                ", color='" + getColor() + '\'' +
+                ", breed=" + getBreed().nameBreed +
+                ", price=" + getPrice() +
+                ", size=" + size +
+                '}';
+    }
+
+    @Override
+    public double getRealMoney() {
+        double total = 0;
+        int percentAge = getAge() / getBreed().longevity * 100;
+        if (percentAge <= 10) {
+            total = getPrice() - getPrice() / 100 * 1;
+        }
+        if (percentAge <= 40 && percentAge > 10) {
+            total = getPrice() - getPrice() / 100 * 2;
+        } else if (percentAge > 40) {
+            total = getPrice() - getPrice() / 100 * 5;
+        }
+        return total;
+    }
 }
--- src/view/Client.java
+++ src/view/Client.java
@@ -1,6 +1,7 @@
 package view;
 
 import controller.PetManager;
+import controller.Validate;
 import model.*;
 
 import java.util.List;
@@ -8,8 +9,9 @@ import java.util.Scanner;
 
 public class Client {
     private static List<Pet> petListClient = PetManager.petList;
+    private static Validate validate = new Validate();
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int choiceMain = -1;
         Scanner inputChoiceMain = new Scanner(System.in);
         while (choiceMain != 0) {
@@ -40,16 +42,140 @@ public class Client {
         }
     }
 
-    public static void addNewPet(){
+    public static void addNewPet() {
         System.out.println("Input type Pet");
         Scanner inputType = new Scanner(System.in);
         try {
             PetType type = PetType.valueOf(inputType.nextLine());
-            Pet  pet = PetFactory.createNewPet(type);
+            Pet pet = PetFactory.createNewPet(type);
             PetManager.addNewPet(pet);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.err.println("We don't have this Pet");
+        }
+    }
+
+    public static void ditPetById() {
+        System.out.println("Input Pet's id need edit: ");
+        String id = validate.checkStringNotNull();
+        int check = -1;
+        if (PetManager.getPetById(id) == check) {
+            System.err.println("Can't find the Pet, Please try again");
+        } else {
+            int index = PetManager.getPetById(id);
+            if (petListClient.get(index) instanceof Cat) {
+                System.out.println("Editing Cat: " + petListClient.get(index).getName());
+                System.out.println("Edit id: " + "(" + petListClient.get(index).getId() + ")");
+                String editId = validate.checkStringNotNull();
+
+                System.out.println("Edit name: " + "(" + petListClient.get(index).getName() + ")");
+                String editName = validate.checkStringNotNull();
+
+                System.out.println("Edit age: " + "(" + petListClient.get(index).getAge() + ")");
+                int editAge = validate.checkInt();
+
+                System.out.println("Edit gender: " + "(" + petListClient.get(index).getGender() + ")");
+                String editGender = validate.checkStringNotNull();
+
+                System.out.println("Edit weight: " + "(" + petListClient.get(index).getWeight() + ")");
+                double editWeight = validate.checkDouble();
+
+                System.out.println("Edit color: " + "(" + petListClient.get(index).getColor() + ")");
+                String editColor = validate.checkStringNotNull();
+
+                System.out.println("Edit name of breed: " + "(" + petListClient.get(index).getBreed().getNameBreed()+ ")");
+                String editNameBreed = validate.checkStringNotNull();
+
+                System.out.println("Edit longevity: " + "(" + petListClient.get(index).getBreed().getLongevity() + ")");
+                int editLongevity = validate.checkInt();
+
+                Breed newBreed = new Breed(editNameBreed, editLongevity);
+
+                System.out.println("Edit price: " + "(" + petListClient.get(index).getPrice() + ")");
+               double editPrice = validate.checkDouble();
+
+                System.out.println("Edit type of leg: " + "(" + ((Cat) petListClient.get(index)).getTypeOfLeg() + ")");
+                String editLeg = validate.checkStringNotNull();
+
+                System.out.println("Edit type of hair: " + "(" + ((Cat) petListClient.get(index)).getTypeOfHair() + ")");
+                String editHair = validate.checkStringNotNull();
+
+                Cat editCat = new Cat(editId, editName, editAge, editGender, editWeight, editColor, newBreed, editPrice, editLeg, editHair );
+                PetManager.editPetByIndex(index, editCat);
+            }
+            if (petListClient.get(index) instanceof Dog) {
+                System.out.println("Editing dog: " + petListClient.get(index).getName());
+                System.out.println("Edit id: " + "(" + petListClient.get(index).getId() + ")");
+                String editId = validate.checkStringNotNull();
+
+                System.out.println("Edit name: " + "(" + petListClient.get(index).getName() + ")");
+                String editName = validate.checkStringNotNull();
+
+                System.out.println("Edit age: " + "(" + petListClient.get(index).getAge() + ")");
+                int editAge = validate.checkInt();
+
+                System.out.println("Edit gender: " + "(" + petListClient.get(index).getGender() + ")");
+                String editGender = validate.checkStringNotNull();
+
+                System.out.println("Edit weight: " + "(" + petListClient.get(index).getWeight() + ")");
+                double editWeight = validate.checkDouble();
+
+                System.out.println("Edit color: " + "(" + petListClient.get(index).getColor() + ")");
+                String editColor = validate.checkStringNotNull();
+
+                System.out.println("Edit name of breed: " + "(" + petListClient.get(index).getBreed().getNameBreed()+ ")");
+                String editNameBreed = validate.checkStringNotNull();
+
+                System.out.println("Edit longevity: " + "(" + petListClient.get(index).getBreed().getLongevity() + ")");
+                int editLongevity = validate.checkInt();
+
+                Breed newBreed = new Breed(editNameBreed, editLongevity);
+
+                System.out.println("Edit price: " + "(" + petListClient.get(index).getPrice() + ")");
+                double editPrice = validate.checkDouble();
+
+                System.out.println("Edit type of ear: " + ((Dog) petListClient.get(index)).getTypeOfEar());
+                String editType = validate.checkStringNotNull();
+
+                Dog editDog = new Dog(editId, editName, editAge, editGender, editWeight, editColor, newBreed, editPrice, editType);
+                PetManager.editPetByIndex(index, editDog);
+            }
+            if (petListClient.get(index) instanceof Mouse) {
+                System.out.println("Editing mouse: " + petListClient.get(index).getName());
+                System.out.println("Edit id: " + "(" + petListClient.get(index).getId() + ")");
+                String editId = validate.checkStringNotNull();
+
+                System.out.println("Edit name: " + "(" + petListClient.get(index).getName() + ")");
+                String editName = validate.checkStringNotNull();
+
+                System.out.println("Edit age: " + "(" + petListClient.get(index).getAge() + ")");
+                int editAge = validate.checkInt();
+
+                System.out.println("Edit gender: " + "(" + petListClient.get(index).getGender() + ")");
+                String editGender = validate.checkStringNotNull();
+
+                System.out.println("Edit weight: " + "(" + petListClient.get(index).getWeight() + ")");
+                double editWeight = validate.checkDouble();
+
+                System.out.println("Edit color: " + "(" + petListClient.get(index).getColor() + ")");
+                String editColor = validate.checkStringNotNull();
+
+                System.out.println("Edit name of breed: " + "(" + petListClient.get(index).getBreed().getNameBreed()+ ")");
+                String editNameBreed = validate.checkStringNotNull();
+
+                System.out.println("Edit longevity: " + "(" + petListClient.get(index).getBreed().getLongevity() + ")");
+                int editLongevity = validate.checkInt();
+
+                Breed newBreed = new Breed(editNameBreed, editLongevity);
+
+                System.out.println("Edit price: " + "(" + petListClient.get(index).getPrice() + ")");
+                double editPrice = validate.checkDouble();
+
+                System.out.println("Edit size: " + "(" + ((Mouse) petListClient.get(index)).getSize() + ")");
+                double editSize = validate.checkDouble();
+
+                Mouse editMouse = new Mouse(editId, editName, editAge, editGender, editWeight, editColor, newBreed, editPrice, editSize);
+                PetManager.editPetByIndex(index, editMouse);
+            }
         }
     }
 }
